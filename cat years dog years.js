95ec00edef8d2899b986aad7191557476e3ba70b
maelaf180 <maelafhabtachin@gmail.com>
2024-08-19 21:51:02 +0300

    solved codewars cat years dog years

---
+++ cat years dog years.js
@@ -0,0 +1,11 @@
+var humanYearsCatYearsDogYears = function (humanYears) {
+  if (humanYears === 1) {
+    return [1, 15, 15];
+  } else if (humanYears === 2) {
+    return [2, 24, 24];
+  } else {
+    let catYears = 15 + 9 + (humanYears - 2) * 4;
+    let dogYears = 15 + 9 + (humanYears - 2) * 5;
+    return [humanYears, catYears, dogYears];
+  }
+};
