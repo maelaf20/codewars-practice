94aeba2ccdc8ba5346c0986f71625955d2cb9678
maelaf180 <maelafhabtachin@gmail.com>
2024-09-03 19:56:41 +0300

    solved codewars calculate bmi

---
+++ Calculate BMI.js
@@ -0,0 +1,7 @@
+function bmi(weight, height) {
+  let result = weight / height ** 2;
+  if (result <= 18.5) return "Underweight";
+  else if (result <= 25.0) return "Normal";
+  else if (result <= 30.0) return "Overweight";
+  else if (result > 30) return "Obese";
+}
