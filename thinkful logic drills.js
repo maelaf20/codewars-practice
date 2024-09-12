8a8f46cc4e762c43fb0628b0ad4e3954832ad0f4
maelaf180 <maelafhabtachin@gmail.com>
2024-09-12 21:39:40 +0300

    solved codewars thinkful logic drills

---
+++ thinkful logic drills.js
@@ -0,0 +1,7 @@
+function updateLight(current) {
+  
+    if(current == 'yellow') return "red"
+    else if(current == "green") return "yellow"
+    else if (current == "red") return "green"
+  
+  }
